6160dcbf9b4c03e34f0c427748a2fb089f4c0c64
Sergey Kondrashov <sergey.kondrashov@jetbrains.com>
2019-02-27 16:47:14 +0300

    Add account categorization

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
@@ -15,22 +17,28 @@ import (
 )
 
 var (
-	date      string
-	keyID     string
-	secretKey string
-	region    string
+	date       string
+	keyID      string
+	secretKey  string
+	configFile string
+	exact      bool
 )
 
 type settings struct {
-	AWSRegion    string `json:"aws_region"`
 	AWSKeyID     string `json:"aws_key_id"`
 	AWSSecretKey string `json:"aws_secret_key"`
 	Date         string `json:"date"`
 }
 
+type config struct {
+	Categories []struct {
+		Name     string   `json:"name"`
+		Accounts []string `json:"accounts"`
+	} `json:"categories"`
+}
+
 type serviceCost struct {
 	AccountID   string `json:"account_id"`
-	Region      string `json:"region"`
 	ServiceName string `json:"service_name"`
 	ServiceCost string `json:"service_cost"`
 	Timestamp   string `json:"timestamp"`
@@ -38,11 +46,17 @@ type serviceCost struct {
 
 func init() {
 	flag.StringVar(&date, "date", "", "date in format yyyy-MM-dd, (by default will be set as yesterday)")
-	flag.StringVar(&region, "region", "eu-central-1", "AWS region(by default 'eu-central-1')")
 	flag.StringVar(&keyID, "key-id", "", "AWS key ID(by default will be taken from env AWS_ACCESS_KEY_ID)")
 	flag.StringVar(&secretKey, "secret", "", "AWS secret key(by default will be taken from env AWS_SECRET_KEY)")
+	flag.StringVar(&configFile, "config", "", "config file")
+	flag.BoolVar(&exact, "exact", false, "show only accounts from config file")
 
 	flag.Parse()
+
+	if exact && configFile == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	if keyID == "" {
 		keyID = os.Getenv("AWS_ACCESS_KEY_ID")
@@ -58,9 +72,27 @@ func init() {
 	}
 
 	if date == "" {
-		yestarday := time.Now().AddDate(0, 0, -1)
-		date = yestarday.Format("2006-01-02")
+		yesterday := time.Now().AddDate(0, 0, -1)
+		date = yesterday.Format("2006-01-02")
 	}
+}
+
+func loadConfig(path string) (config, error) {
+	var (
+		c   config
+		err error
+	)
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return c, fmt.Errorf("can't load config: %v", err)
+	}
+
+	err = json.Unmarshal(file, &c)
+	if err != nil {
+		return c, fmt.Errorf("can't unmarshal config: %v", err)
+	}
+
+	return c, err
 }
 
 func getDataFromAWS(a *settings) (*[]costexplorer.ResultByTime, error) {
@@ -106,7 +138,7 @@ func getDataFromAWS(a *settings) (*[]costexplorer.ResultByTime, error) {
 	return &data.ResultsByTime, err
 }
 
-func getServiceCost(results *[]costexplorer.ResultByTime, region string) []serviceCost {
+func getServiceCost(results *[]costexplorer.ResultByTime) []serviceCost {
 	sc := []serviceCost{}
 	t, _ := time.Parse("2006-01-02", date)
 	timestamp := strconv.FormatInt(t.UnixNano(), 10)
@@ -114,8 +146,8 @@ func getServiceCost(results *[]costexplorer.ResultByTime, region string) []servi
 	for _, timePeriod := range *results {
 		for _, group := range timePeriod.Groups {
 			sc = append(sc, serviceCost{
-				AccountID:   group.Keys[1],
-				Region:      region,
+				AccountID: group.Keys[1],
+				// Region:      region,
 				ServiceName: strings.Replace(group.Keys[0], " ", "_", -1),
 				ServiceCost: *group.Metrics["UnblendedCost"].Amount,
 				Timestamp:   timestamp,
@@ -126,15 +158,53 @@ func getServiceCost(results *[]costexplorer.ResultByTime, region string) []servi
 	return sc
 }
 
-func printInfluxLineProtocol(sc []serviceCost) {
-	for _, s := range sc {
-		fmt.Printf("aws-cost,account_id=%v,region=%v,service_name=%v cost=%v %v\n", s.AccountID, s.Region, s.ServiceName, s.ServiceCost, s.Timestamp)
+func printInfluxLineProtocol(sc []serviceCost, c config) {
+	if len(c.Categories) > 0 {
+		for _, s := range sc {
+			for _, category := range c.Categories {
+				if exact {
+					if checkElementInArray(category.Accounts, s.AccountID) {
+						fmt.Printf("aws-cost,account_id=%v,service_name=%v,category=%v cost=%v %v\n", s.AccountID, s.ServiceName, category.Name, s.ServiceCost, s.Timestamp)
+					}
+				} else {
+					if checkElementInArray(category.Accounts, s.AccountID) {
+						fmt.Printf("aws-cost,account_id=%v,service_name=%v,category=%v cost=%v %v\n", s.AccountID, s.ServiceName, category.Name, s.ServiceCost, s.Timestamp)
+					} else {
+						fmt.Printf("aws-cost,account_id=%v,service_name=%v cost=%v %v\n", s.AccountID, s.ServiceName, s.ServiceCost, s.Timestamp)
+					}
+				}
+			}
+		}
+	} else {
+		for _, s := range sc {
+			fmt.Printf("aws-cost,account_id=%v,service_name=%v cost=%v %v\n", s.AccountID, s.ServiceName, s.ServiceCost, s.Timestamp)
+		}
 	}
 }
 
+func checkElementInArray(array []string, e string) bool {
+	for _, ae := range array {
+		if ae == e {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
+	var (
+		c   config
+		err error
+	)
+
+	if configFile != "" {
+		c, err = loadConfig(configFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	data, err := getDataFromAWS(&settings{
-		AWSRegion:    region,
 		AWSKeyID:     keyID,
 		AWSSecretKey: secretKey,
 		Date:         date,
@@ -143,5 +213,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	printInfluxLineProtocol(getServiceCost(data, region))
+	printInfluxLineProtocol(getServiceCost(data), c)
 }
